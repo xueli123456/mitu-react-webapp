aa291d9215e75232e62ae20b36d2983e7cd9316d
unknown <1441345595@qq.com>
2019-11-20 13:12:34 +0800

    react-home

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+export default class App extends Component{
+    // constructor(props){
+    //     super()
+    // }
+    render() {
+        return(
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
